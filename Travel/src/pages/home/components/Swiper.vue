f75506547b1ef0134deee973c8c0eda1fd4fbc5c
xiaohuoWEB <xhJSzc@1>
2018-08-26 23:01:49 +0800

    20180826 vue仿去哪儿网

---
+++ Travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeSwiper',
+    props: {
+      list: Array
+    },
+    data () {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          loop: true,
+          autoplay: 3000,
+          speed: 300
+        }
+      }
+    },
+    computed: {
+      showSwiper () {
+        return this.list.length
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  // 三个 >>> 表示样式穿透 所有.wrapper 子组件中的 .swiper-pagination-bullet-active设置为这个样式
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff;
+  .wrapper
+    width 100%;
+    height 0;
+    overflow hidden;
+    padding-bottom 31.25%;
+    background #ffffff;
+    .swiper-img
+      width 100%;
+      height: 31.25vw;
+</style>
