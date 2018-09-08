385ac18a0d30ae50f914ee9e546ae15baa6aecfa
xiaohuoWEB <xhJSzc@1>
2018-09-08 20:30:15 +0800

    添加城市列表及数据请求，字母列表拖动

---
+++ Travel/src/pages/city/City.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+    <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
+    <city-alphabet @change="handleLetterChange" :cities="cities"></city-alphabet>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  import CityHeader from './components/Header'
+  import CitySearch from './components/Search'
+  import CityList from './components/List'
+  import CityAlphabet from './components/Alphabet'
+  export default {
+    name: 'City',
+    components: {
+      CityHeader,
+      CitySearch,
+      CityList,
+      CityAlphabet
+    },
+    data () {
+      return {
+        cities: {},
+        hotCities: [],
+        letter: ''
+      }
+    },
+    mounted() {
+      this.getCityInfo()
+    },
+    methods: {
+      getCityInfo() {
+        axios.get('/api/city.json').then(this.handleGetCityInfoSucc)
+      },
+      handleGetCityInfoSucc (res) {
+        if (res.data.ret && res.data) {
+          const data = res.data.data
+          this.hotCities = data.hotCities
+          this.cities = data.cities
+        }
+      },
+      handleLetterChange(letter) {
+        this.letter = letter
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- Travel/src/pages/home/components/Header.vue
+++ Travel/src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -31,7 +33,7 @@
   */
   @import '~styles/varibles.styl'
   .header
-    line-height: .86rem;
+    line-height: $headerHeight;
     display: flex;
     background: $bgColor;
     color: #fff
@@ -55,6 +57,7 @@
       width:1.24rem;
       float: right;
       text-align: center;
+      color: #fff;
       .arrow-icon
         margin-left: -0.04rem;
         font-size: .24rem;
